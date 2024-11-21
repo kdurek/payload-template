d5db90c14bac98913edaecc27b377e55e5ff58f9
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2024-11-21 12:54:02 +0100

    refactor: streamline media upload settings and adjust CSS variable order

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,9 @@ const config = {
   theme: {
     container: {
       center: true,
-      padding: '2rem',
+      padding: '1.5rem',
       screens: {
-        '2xl': '1400px',
+        xl: '1280px',
       },
     },
     extend: {
